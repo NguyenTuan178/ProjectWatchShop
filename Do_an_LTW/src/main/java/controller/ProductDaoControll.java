0c39f007953d77d55327fd09e99815e8f8c6f021
Nguyen Van Tuan <73975734+NguyenTuan178@users.noreply.github.com>
2021-01-03 21:06:27 +0700

    code website
    push code ltw

---
+++ Do_an_LTW/src/main/java/controller/ProductDaoControll.java
@@ -0,0 +1,22 @@
+package controller;
+import bean.ProductDao;
+import enity.ProductDao1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+
+@WebServlet(urlPatterns = "/chitietsanpham")
+public class ProductDaoControll extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+}
